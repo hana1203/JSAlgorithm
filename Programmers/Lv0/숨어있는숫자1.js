980252bd7cc36862b6c5705dcc3a6fb1d2a98d67
hana1203 <hanaindec@gmail.com>
2022-11-03 23:30:34 +0900

    solved with string split, filter, reduce

--- Programmers/Lv0/숨어있는숫자1.js
+++ Programmers/Lv0/숨어있는숫자1.js
@@ -1,21 +1,25 @@
 function solution(my_string) {
-  let arr = my_string.split(""); //['a', 'A', 'b', '1','B', '2', 'c', 'C', '3', '4', 'o', 'O','p']
-  let newArr = [];
-  for (let el of arr) {
-    if (Number(el) % 1 === 0) {
-      newArr.push(Number(el));
-    }
-  }
-  return newArr.reduce((acc, cur) => acc + cur);
+  //   let arr = my_string.split(""); //['a', 'A', 'b', '1','B', '2', 'c', 'C', '3', '4', 'o', 'O','p']
+  //   let newArr = [];
+  //   for (let el of arr) {
+  //     if (Number(el) % 1 === 0) {
+  //       newArr.push(Number(el));
+  //     }
+  //   }
+  //   return newArr.reduce((acc, cur) => acc + cur);
 
-  //시도
-  //   let arr = my_string.split("").map((el) => {
-  //     Number(el); //여기서 number형으로 바뀌지 않음
-  //   });
+  //다른 방법
+  let arr = my_string.split("").map((el) => Number(el));
   //   console.log(arr);
+  //   [
+  //     NaN, NaN, NaN, 1, NaN,
+  //       2, NaN, NaN, 3,   4,
+  //     NaN, NaN, NaN
+  //   ]
   //   let nums = arr.filter((el) => typeof el !== "NaN");
-  //   console.log("nums", nums);
-  //   return nums.reduce((acc, cur) => acc + cur);
+  let nums = arr.filter((el) => el % 1 === 0);
+  //   console.log("nums", nums); //nums [ 1, 2, 3, 4 ]
+  return nums.reduce((acc, cur) => acc + cur);
 }
 
 console.log(solution("aAb1B2cC34oOp")); //10
